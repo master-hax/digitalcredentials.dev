85f40503a044a188520d29b715205b0f66cda25d
Lee Campbell <leecam@google.com>
2024-09-19 13:42:23 -0700

    Fix editUrl in the config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/digitalcredentialsdev/dev.digitalcredentials.dev/tree/main/',
+            'https://github.com/digitalcredentialsdev/digitalcredentials.dev/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
