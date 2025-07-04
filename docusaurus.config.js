6bdfdd089ddf8329e9768743babb6733426b0154
Mohamed Amir Yosef <mamir@chromium.org>
2025-07-04 11:11:00 +0200

    Use single qoutes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            `https://github.com/digitalcredentialsdev/digitalcredentials.dev/blob/main/`
+            'https://github.com/digitalcredentialsdev/digitalcredentials.dev/blob/main/'
         },
         blog: {
           showReadingTime: true,
